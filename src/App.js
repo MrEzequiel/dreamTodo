224c31ec715f2753fb2db73a91ea95c89d6bd286
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2022-01-08 23:07:10 -0500

    :ok_hand: context todo list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useReducer } from 'react'
 import { ThemeProvider } from 'styled-components'
 import FormTodo from './components/FormTodo/index'
 import TodoList from './components/TodoList'
@@ -6,34 +5,18 @@ import TodoList from './components/TodoList'
 import GlobalStyles from './styles/GlobalStyles'
 import TitleStyle from './styles/TitleStyle'
 import darkTheme from './styles/theme/dark'
-
-export const ACTIONS = {
-  ADD_TODO: 'add-todo'
-}
-
-function newTodo(name) {
-  return { id: Date.now(), name, complete: false }
-}
-
-function reducer(todos, action) {
-  switch (action.types) {
-    case ACTIONS.ADD_TODO:
-      return [newTodo(action.payload.name), ...todos]
-    default:
-      return todos
-  }
-}
+import TodoListProvider from './context/TodoListContext'
 
 function App() {
-  const [todos, dispatch] = useReducer(reducer, [])
-
   return (
-    <ThemeProvider theme={darkTheme}>
-      <TitleStyle>To Do</TitleStyle>
-      <FormTodo dispatch={dispatch} />
-      {!!todos.length && <TodoList todos={todos} />}
-      <GlobalStyles />
-    </ThemeProvider>
+    <TodoListProvider>
+      <ThemeProvider theme={darkTheme}>
+        <TitleStyle>To Do</TitleStyle>
+        <FormTodo />
+        <TodoList />
+        <GlobalStyles />
+      </ThemeProvider>
+    </TodoListProvider>
   )
 }
 
