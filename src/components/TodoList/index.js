7654686b032af433f0fa2d8ce1168e039f3dce21
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2022-01-09 00:14:12 -0500

    :sparkles: todo list for complet

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -7,19 +7,37 @@ function TodoList() {
   const { todos } = useContext(TodoListContext)
   if (!todos.length) return null
 
-  // const todosCompleted = todos.filter(todo => todo.complete)
+  const todosCompleted = todos.filter(todo => todo.complete)
+  const todosNotCompleted = todos.filter(todo => !todo.complete)
 
   return (
     <s.TodoWrapper>
-      <h2>
-        {todos.length > 1 ? 'Tasks' : 'Task'} - {todos.length}
-      </h2>
+      {!!todosNotCompleted.length && (
+        <>
+          <h2>
+            {todosNotCompleted.length > 1 ? 'Tasks' : 'Task'} -{' '}
+            {todosNotCompleted.length}
+          </h2>
 
-      <s.TodoListWrapper>
-        {todos.map(todo => (
-          <Todo key={todo.id} todo={todo} />
-        ))}
-      </s.TodoListWrapper>
+          <s.TodoListWrapper>
+            {todosNotCompleted.map(todo => (
+              <Todo key={todo.id} todo={todo} />
+            ))}
+          </s.TodoListWrapper>
+        </>
+      )}
+
+      {!!todosCompleted.length && (
+        <>
+          <h2>Completed - {todosCompleted.length}</h2>
+
+          <s.TodoListWrapper>
+            {todosCompleted.map(todo => (
+              <Todo key={todo.id} todo={todo} />
+            ))}
+          </s.TodoListWrapper>
+        </>
+      )}
     </s.TodoWrapper>
   )
 }
--- src/components/TodoList/styles.js
+++ src/components/TodoList/styles.js
@@ -3,7 +3,10 @@ import { Container } from '../../styles/LayoutComponents'
 
 export const TodoWrapper = styled.div`
   ${Container()}
-  margin-top: 40px;
+
+  h2 {
+    margin: 40px 0 20px;
+  }
 `
 
 export const TodoListWrapper = styled.ul`
