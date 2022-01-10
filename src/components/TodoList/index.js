b0c95546d966cce92a6d2ed68d9855b22db5f90e
unknown <mr.ezequiel.soares@gmail.com>
2022-01-10 11:41:30 -0300

    :sparkles: empty todo list message

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,10 +1,12 @@
 import useTodo from '../../hooks/useTodo'
 import Todo from '../Todo'
+import EmptyTodo from './EmptyTodo'
 import * as s from './styles'
 
 function TodoList() {
   const { todos } = useTodo()
-  if (!todos.length) return null
+
+  if (!todos.length) return <EmptyTodo />
 
   const todosCompleted = todos.filter(todo => todo.complete)
   const todosNotCompleted = todos.filter(todo => !todo.complete)
