e89fb15334e5aedb871e1e1645b1810cbc76ae57
Italo Ferreira <grd788@gmail.com>
2022-02-25 15:04:26 -0300

    :books: init documentation endpoints with Swagger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,10 @@ import express, { NextFunction, Request, Response } from 'express';
 import { AppError } from './infra/errors/AppError';
 import 'express-async-errors'
 import { routes } from './routes/routes';
-import 'dotenv'
+import 'dotenv';
+import SwaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
+
 
 
 const app = express();
@@ -11,21 +14,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(routes);
 
+
 app.use((err: Error, request: Request, response: Response, next:NextFunction) =>{
   if(err instanceof AppError){
-      return response.status(err.statusCode).json({
+    return response.status(err.statusCode).json({
         message: err.message
-          
+        
       })
       
-  }
-
-  return response.status(500).json({
+    }
+    
+    return response.status(500).json({
       status: "error",
       message: `Internal server Error - ${err.message}`
+    })
   })
-})
+  
+  app.listen(3333, () => {
+    console.log('Server is running in port 3333')
+  })
 
-app.listen(3333, () => {
-  console.log('Server is running in port 3333')
-})
+  app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(swaggerDocument));
