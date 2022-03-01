bf91ff5e852208e6d7d5c8bb7a11549df7ccf4bd
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2022-03-01 08:44:42 -0500

    feat: :sparkles: creating axios interceptor

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,9 +1,20 @@
 import axios from 'axios'
+import Cookies from 'js-cookie'
 
 const baseURL = process.env.REACT_APP_API_URL ?? 'http://localhost:3333'
 
 const api = axios.create({
   baseURL
+})
+
+api.interceptors.request.use(async config => {
+  const token = Cookies.get('auth')
+
+  if (token) {
+    config.headers = { Authorization: token }
+  }
+
+  return config
 })
 
 export default api
