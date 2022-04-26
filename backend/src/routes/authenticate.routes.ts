5271c4b76784eea1a869346c027e3e04e735da85
Italo Ferreira <grd788@gmail.com>
2022-04-26 17:42:54 -0300

    settings :gear: dayjs dependecyy and routes alters

--- backend/src/routes/authenticate.routes.ts
+++ backend/src/routes/authenticate.routes.ts
@@ -1,13 +1,17 @@
 import Router from 'express';
 import { AuthenticateUserController } from '../modules/accounts/useCases/authenticateUser/AuthenticateUserController';
 import { AuthenticateUserGoogleController } from '../modules/accounts/useCases/authenticateUserGoogle/AuthenticateUserGoogleController';
+import { RefreshTokenController } from '../modules/accounts/useCases/refreshToken/RefreshTokenController';
 
 const authRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController()
+const refreshTokenController = new RefreshTokenController()
 const authenticateUserGoogleController = new AuthenticateUserGoogleController()
 
 authRoutes.post('/login', authenticateUserController.handle);
+authRoutes.post('/refreshToken', refreshTokenController.handle);
 authRoutes.post('/login/google/:CLIENT_ID/:token', authenticateUserGoogleController.handle);
+
 
 export { authRoutes }
