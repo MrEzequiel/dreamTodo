7453a79210659bd35c4f914290f17a45e181fdf4
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2022-04-26 17:57:36 -0500

    refactor: :recycle: using react hook form to validation sign in form

---
+++ frontend/src/pages/Register/validations.ts
@@ -0,0 +1,9 @@
+import * as yup from 'yup'
+
+export const registerValidation = yup.object().shape({
+  email: yup.string().email('Invalid email').required('Email is required'),
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(6, 'Password must be at least 6 characters')
+})
