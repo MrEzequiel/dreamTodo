9c17d3808c66d38746c161ece473c0cc45a699d2
Italo Ferreira <grd788@gmail.com>
2022-05-11 15:35:06 -0300

    feat :sparkles: repositories for method's and prisma repository implementation

---
+++ backend/src/repositories/UserRepositories/userRepositories.ts
@@ -0,0 +1,34 @@
+import { RefreshToken, User } from '@prisma/client'
+import { ICreateUserDTO } from '../../modules/accounts/dtos/ICreateUserDTO'
+import { RefreshTokenDTO } from '../../modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase'
+import { IRequestEdit } from '../../modules/accounts/useCases/editUser/EditUserUseCase'
+
+export interface UserRepository {
+  create: ({
+    id,
+    name,
+    email,
+    password,
+    imageProfile,
+    imageURL
+  }: ICreateUserDTO) => Promise<User>
+
+  createRefreshToken: ({
+    userId,
+    refreshToken,
+    expires_in
+  }: RefreshTokenDTO) => Promise<RefreshToken>
+
+  findUserByEmail: (email: string) => Promise<User | null>
+
+  findUserById: (id: string) => Promise<User | null>
+
+  editUser: ({
+    id,
+    name,
+    imageURL,
+    imageProfile
+  }: IRequestEdit) => Promise<User>
+
+  editUserPassword: (id: string, passowrd: string) => Promise<void>
+}
