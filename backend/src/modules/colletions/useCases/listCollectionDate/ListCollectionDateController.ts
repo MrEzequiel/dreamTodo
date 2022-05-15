4517e840c4398bccb0f843ab176aa8af04cbe6ec
Italo Ferreira <grd788@gmail.com>
2022-05-14 23:44:28 -0300

    refactor :recycle: alter repository method

--- backend/src/modules/colletions/useCases/listCollectionDate/ListCollectionDateController.ts
+++ backend/src/modules/colletions/useCases/listCollectionDate/ListCollectionDateController.ts
@@ -1,19 +1,13 @@
-import { Request, Response } from "express";
-import { ListCollectionDateUseCase } from "./ListCollectionDateUseCase";
-
-
-
-
+import { Request, Response } from 'express'
+import { ListCollectionDateUseCase } from './ListCollectionDateUseCase'
 
 export class ListCollectionDateController {
-
-  async handle(request: Request, response: Response): Promise<Response>{
-
+  async handle(request: Request, response: Response): Promise<Response> {
     const { modo } = request.query
 
-    const listCollectionUseCase = new ListCollectionDateUseCase();
+    const listCollectionUseCase = new ListCollectionDateUseCase()
 
-    const todo = await listCollectionUseCase.execute(String(modo));
+    const todo = await listCollectionUseCase.execute(String(modo))
 
     return response.json(todo)
   }
