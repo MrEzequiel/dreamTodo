c04298c7a14711f7bdcf85ca40c0a4f9d5677592
Italo Ferreira <grd788@gmail.com>
2022-05-14 23:44:02 -0300

    refactor :recycle: alter repository method

--- backend/src/modules/colletions/useCases/listCollection/ListCollectionController.ts
+++ backend/src/modules/colletions/useCases/listCollection/ListCollectionController.ts
@@ -1,17 +1,14 @@
-import { Request, Response } from "express";
-import { ListCollectionUseCase } from "./ListCollectionUseCase";
-
+import { Request, Response } from 'express'
+import { ListCollectionUseCase } from './ListCollectionUseCase'
 
 export class ListCollectionController {
-
-  async handle(request: Request, response: Response): Promise<Response>{
-
+  async handle(request: Request, response: Response): Promise<Response> {
     const { id: user_id } = request.user
 
-    const listCollectionUseCase = new ListCollectionUseCase();
+    const listCollectionUseCase = new ListCollectionUseCase()
 
-    const collection = await listCollectionUseCase.execute(user_id as string);
+    const collection = await listCollectionUseCase.execute(user_id as string)
 
-    return response.json(collection);
+    return response.json(collection)
   }
 }
