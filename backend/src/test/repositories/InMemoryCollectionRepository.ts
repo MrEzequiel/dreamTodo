5dee49a68f6c64e6f1eee8a4dff9f7aaea89b3a6
Italo Ferreira <grd788@gmail.com>
2022-05-15 23:39:16 -0300

    test :test_tube: test repositories in memory

--- backend/src/test/repositories/InMemoryCollectionRepository.ts
+++ backend/src/test/repositories/InMemoryCollectionRepository.ts
@@ -1,5 +1,6 @@
 import { Collection } from '@prisma/client'
 import { CollectionRepository } from '../../repositories/CollectionRepositories/collectionRepositories'
+import { v4 } from 'uuid'
 
 export type RequestCreateColletion = {
   userId: string
@@ -8,20 +9,27 @@ export type RequestCreateColletion = {
 }
 
 export class InMemoryCollectionRepository implements CollectionRepository {
+  listTodoOfColletion: (name: string, complete: string) => Promise<any>
   colletions: Collection[] = []
 
   async createCollection(data: RequestCreateColletion) {
-    Object.assign(data)
-    
+    Object.assign(data, {
+      id: v4()
+    })
+
     this.colletions.push(data as Collection)
     return data as Collection
   }
-  deleteColletion: (id: string) => Promise<void>
+  async deleteColletion(id: string) {
+    this.colletions.filter(colletion => colletion.id === id)
+  }
+
   editColletion: (
     id: string,
     name: string,
     emoji: string
   ) => Promise<Collection>
+
   async findCollectionByName(name: string) {
     const colletion = this.colletions.find(colletion => colletion.name === name)
     return colletion ?? null
