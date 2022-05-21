d535776bc90b2fe6b92142c35ea6d4a38ea6afb6
Italo Ferreira <grd788@gmail.com>
2022-05-21 12:42:34 -0300

    test :test_tube: create user id

--- backend/src/modules/accounts/useCases/createUser/CreateUserUseCase.spec.ts
+++ backend/src/modules/accounts/useCases/createUser/CreateUserUseCase.spec.ts
@@ -9,7 +9,6 @@ describe('Create a user', () => {
   it('should be able to create a new user', async () => {
     await expect(
       createUser.execute({
-        id: 'bf1bb7e5-aa4d-4c4f-8890-239404c2e178',
         name: 'test1',
         email: 'test@test.com',
         password: '1234',
